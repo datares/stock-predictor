839573a061bd0b3b1a54d20bff0c30ec5cb5eb68
Amrith Deepak <amrithdeepak@Amriths-MBP-2.lan1>
2019-10-28 19:28:17 -0700

    first_commit

--- run.py
+++ run.py
@@ -1,6 +1,14 @@
 from lib.utils import preproc_pipeline
 import pandas as pd
+import logging
+logger = logging.getLogger('spam_application')
+logger.setLevel(logging.DEBUG)
 
 if __name__ == "__main__":
-    data = pd.read_csv("<path/to/data>")
-    train_set, validation_set, test_set = preproc_pipeline(data)
+    #data = pd.read_csv("<path/to/data>")
+    #train_set, validation_set, test_set = preproc_pipeline(data)
+
+
+	logger.info('testing info')
+
+	logger.error('testing error')
