2094dee1f4f2dbfbd109135cebd205aedfd96858
Chingyi Ie <chingyiie@Chingyis-MacBook-Pro.local>
2019-11-01 18:17:17 -0700

    added common functions

--- src/preprocess/common.py
+++ src/preprocess/common.py
@@ -2,7 +2,9 @@ import numpy as np
 import pandas as pd
 import os
 
-from sklearn.preprocessing import StandardScaler
+import numpy as np
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.model_selection import train_test_split
 
 # Function definition
 def create_data(file_list):
@@ -19,21 +21,45 @@ def create_data(file_list):
     return pd.DataFrame(df_list)
 
 def preprocess(data):
-    # scaling data
-    scaler = StandardScaler()
-    scaled = scaler.fit_transform(data.reshape(-1, 1))
-    return scaled
+    """
+    takes in a dataframe and returns a normalized train test split
+    """
+    col_names = ['Open', 'High', 'Low', 'Close', 'Volume']
 
-def window_data(data, window_size):
-    # windowing the data
-    X, Y = [], []
-    i = 0
-    while (i + window_size < len(data)):
-        # data points
-        X.append(data[i:i+window_size])
-        # next number in sequence we are trying to predict
-        Y.append(data[i+window_size])
+    # splits the data into training and testing set without shuffling (time series)
+    df_train, df_test = train_test_split(data, train_size=0.8, test_size=0.2, shuffle=False)
 
-        i += 1
-    assert len(X) == len(Y)
-    return X, Y
+    # subsets the data and get values
+    X = df_train.loc[:,col_names].values
+
+    # scales the data
+    min_max_scaler = MinMaxScaler()
+    df_train = min_max_scaler.fit_transform(X)
+    df_test = min_max_scaler.transform(df_test.loc[:,col_names])
+
+    return (df_train, df_test)
+
+def build_timeseries(mat, y_col_index, time_steps):
+    # y_col_index is the index of column that would act as output column
+    # total number of time-series samples would be len(mat) - TIME_STEPS
+    dim_0 = mat.shape[0] - time_steps
+    dim_1 = mat.shape[1]
+    # input is in shape [batch_size (rows), timesteps, features (cols)]
+    x = np.zeros((dim_0, time_steps, dim_1))
+    y = np.zeros((dim_0,))
+    
+    for i in range(dim_0):
+        x[i] = mat[i:time_steps+i]
+        y[i] = mat[time_steps+i, y_col_index]
+    print("length of time-series i/o",x.shape,y.shape)
+    return x, y
+
+def trim_dataset(mat, batch_size):
+    """
+    trims dataset to a size that's divisible by the batch size
+    """
+    no_of_rows_drop = mat.shape[0]%batch_size
+    if(no_of_rows_drop > 0):
+        return mat[:-no_of_rows_drop]
+    else:
+        return mat
