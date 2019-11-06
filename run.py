b26cd9a2ab3205f917a43649409d7e6d7574717e
mrcolo <colo28197@gmail.com>
2019-11-05 21:37:21 -0800

    fixed run.py

--- run.py
+++ run.py
@@ -2,5 +2,5 @@ from lib.utils import preproc_pipeline
 import pandas as pd
 
 if __name__ == "__main__":
-    data = pd.read_csv("<path/to/data>")
+    data = pd.read_csv("./data/00.csv")
     train_set, validation_set, test_set = preproc_pipeline(data)
