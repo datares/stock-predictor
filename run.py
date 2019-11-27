8e0946153eb1fc4717fcfb8de9e824730a8b8788
Chingyi Ie <iechingyi@yahoo.com>
2019-11-26 20:22:18 -0800

    run.py works now

--- run.py
+++ run.py
@@ -1,18 +1,19 @@
 import pandas as pd
 import numpy as np
-from lib.utils import preproc_pipeline, create_data, model_preproc_pipeline, scale_df
+from lib.utils import preproc_pipeline, create_data, model_preproc_pipeline, scale_df, trim_dataset
 from lib.model import setup_model, train_model, validate_model
 
 if __name__ == "__main__":
 
     # CREATING DATA
-    data = create_data(["aapl.us.txt"])
+    stock = create_data(["data/Stocks/aapl.us.txt"])
+    data = stock.iloc[:,1:2]
 
     # DEFINING HYPERPARAMETERS
-    TIME_STEPS = 60
-    BATCH_SIZE = 100
-    lr = 0.00010000 # learning rate
-    EPOCHS = 25
+    TIME_STEPS = 100
+    BATCH_SIZE = 128
+    lr = 0.0001 # learning rate
+    EPOCHS = 10
 
     # PREPROCESSING
     train_set, test_set = preproc_pipeline(data, False) # This returns train and test set
@@ -21,4 +22,12 @@ if __name__ == "__main__":
     # SETTING UP THE MODEL AND TRAINING
     regressor = setup_model(BATCH_SIZE, TIME_STEPS)
     regressor = train_model(regressor, x_train, y_train, EPOCHS, BATCH_SIZE, lr)
+
+    # MAKING PREDICTIONS
+    df_test = trim_dataset(test_set, BATCH_SIZE)
+    df_val, df_testing = np.split(df_test, 2)
+
+    results = validate_model(df_val, regressor, TIME_STEPS)
+
+    print(results)
 
