e2e35ff6bbf19b41a5f9e11672794ae0b626b028
mrcolo <colo28197@gmail.com>
2019-12-02 17:27:20 -0800

    end of fall 2019

--- config.py
+++ config.py
@@ -2,6 +2,6 @@
 class Config(object):
     def __init__(self):
         self.look_back = 10
-        self.batch_size = 128
+        self.batch_size = 256
         self.lr = 0.0001
-        self.epochs = 5
+        self.epochs = 10
